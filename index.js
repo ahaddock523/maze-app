7ccc16bba412cfca77075b25db381c7f83653463
Andrew Haddock <ahaddock523@gmail.com>
2020-01-30 15:31:13 -0700

    created basic cell matrix and implemented first steps of function to determine what neighboring cells to move to for maze creation

--- index.js
+++ index.js
@@ -31,6 +31,20 @@ const walls = [
 World.add(world, walls);
 
 // Maze generation
+const shuffle = (arr) => {
+	let counter = arr.length;
+	while (counter > 0) {
+		const index = Math.floor(Math.random() * counter);
+
+		counter--;
+
+		const temp = arr[counter];
+		arr[counter] = arr[index];
+		arr[index] = temp;
+	}
+	return arr;
+};
+
 const grid = Array(cells).fill(null).map(() => Array(cells).fill(false));
 
 const verticals = Array(cells).fill(null).map(() => Array(cells - 1).fill(false));
@@ -39,4 +53,55 @@ const horizontals = Array(cells - 1).fill(null).map(() => Array(cells).fill(fals
 
 const startRow = Math.floor(Math.random() * cells);
 const startColumn = Math.floor(Math.random() * cells);
-console.log(startRow, startColumn);
+
+const iterateThroughCells = (row, column) => {
+	// If I have visited the cell at [row, column], then return
+	if (grid[row][column]) {
+		return;
+	}
+	// Mark this cell as being visited
+	grid[row][column] = true;
+	// Assemble randomly ordered list of neighbors
+	const neighbors = shuffle([
+		[
+			row - 1,
+			column,
+			'up'
+		],
+		[
+			row,
+			column + 1,
+			'right'
+		],
+		[
+			row + 1,
+			column,
+			'down'
+		],
+		[
+			row,
+			column - 1,
+			'left'
+		]
+	]);
+	// For each neighbor...
+	for (let neighbor of neighbors) {
+		const [
+			nextRow,
+			nextColumn,
+			direction
+		] = neighbor;
+		// See if that neighbor is out of bounds
+		if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+			continue;
+		}
+		// If we have visisted that neighbor, continue to next neighbor
+		if (grid[nextRow][nextColumn]) {
+			continue;
+		}
+		// Remove a wall from either horizontals or verticals
+	}
+	// Visit next cell
+};
+
+iterateThroughCells(1, 1);
