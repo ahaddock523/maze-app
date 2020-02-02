29b1b24a576efdb8a6b4ea36ebc476f3dd3eb7b7
Andrew Haddock <ahaddock523@gmail.com>
2020-02-02 16:39:11 -0700

    added random wall generation for horizontal and vertical walls in 3x3 grid space

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cells = 3;
 const width = 600;
 const height = 600;
 
+const unitLength = width / cells;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
@@ -100,8 +102,57 @@ const iterateThroughCells = (row, column) => {
 			continue;
 		}
 		// Remove a wall from either horizontals or verticals
+		if (direction === 'left') {
+			verticals[row][column - 1] = true;
+		} else if (direction === 'right') {
+			verticals[row][column] = true;
+		} else if (direction === 'up') {
+			horizontals[row - 1][column] = true;
+		} else if (direction === 'down') {
+			horizontals[row][column] = true;
+		}
+
+		// Visit next cell
+		iterateThroughCells(nextRow, nextColumn);
 	}
-	// Visit next cell
 };
 
-iterateThroughCells(1, 1);
+iterateThroughCells(startRow, startColumn);
+
+horizontals.forEach((row, rowIndex) => {
+	row.forEach((open, columnIndex) => {
+		if (open) {
+			return;
+		}
+
+		const wall = Bodies.rectangle(
+			columnIndex * unitLength + unitLength / 2,
+			rowIndex * unitLength + unitLength,
+			unitLength,
+			10,
+			{
+				isStatic : true
+			}
+		);
+		World.add(world, wall);
+	});
+});
+
+verticals.forEach((row, rowIndex) => {
+	row.forEach((open, columnIndex) => {
+		if (open) {
+			return;
+		}
+
+		const wall = Bodies.rectangle(
+			columnIndex * unitLength + unitLength,
+			rowIndex * unitLength + unitLength / 2,
+			10,
+			unitLength,
+			{
+				isStatic : true
+			}
+		);
+		World.add(world, wall);
+	});
+});
